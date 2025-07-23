0b110f4d0ef4dd811b672cdd6bc6e72a686cabe6
Mattia PARRINELLO <contactperso.mp@gmail.com>
2025-07-23 19:50:58 +0200

    Daily push

--- script.js
+++ script.js
@@ -3,16 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('mobile-menu');
 
     toggleBtn.addEventListener('click', () => {
-        toggleBtn.classList.toggle('active');
-
-        const isOpen = toggleBtn.classList.contains('active');
-
-        if (isOpen) {
-            menu.classList.remove('opacity-0', 'scale-95', 'pointer-events-none');
-            menu.classList.add('opacity-100', 'scale-100');
-        } else {
-            menu.classList.add('opacity-0', 'scale-95', 'pointer-events-none');
-            menu.classList.remove('opacity-100', 'scale-100');
-        }
+        const isOpen = toggleBtn.classList.toggle('active');
+        menu.classList.toggle('active', isOpen);
     });
 });
